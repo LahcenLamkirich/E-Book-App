4d6b481367721be4ea1ef29f37213d34b5172e6a
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-02-12 10:24:44 +0100

    commit

--- lib/register.dart
+++ lib/register.dart
@@ -12,33 +12,98 @@ class _RegisterState extends State<Register> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 10,),
-            Image.asset('assets/images/login.png', width: 200, height: 260,),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              alignment: Alignment.center,
-              child: Form(
-                child: TextFormField(
-                  maxLength: 50,
-                  decoration: InputDecoration(
-                    hintText: "Username",
-                    labelText: "Username",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(color: Colors.grey)
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(height: 10,),
+              Image.asset('assets/images/login.png', width: 200, height: 260,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.center,
+                child: Form(
+                  child: TextFormField(
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      labelText: "Username",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.grey)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius:BorderRadius.circular(40),
+                        borderSide: BorderSide(color: Colors.blue),
+                      )
                     ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius:BorderRadius.circular(40),
-                      borderSide: BorderSide(color: Colors.blue),
-                    )
                   ),
                 ),
               ),
-            )
-          ],
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.center,
+                child: Form(
+                  child: TextFormField(
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                        hintText: "Username",
+                        labelText: "Username",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide(color: Colors.grey)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:BorderRadius.circular(40),
+                          borderSide: BorderSide(color: Colors.blue),
+                        )
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.center,
+                child: Form(
+                  child: TextFormField(
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                        hintText: "Username",
+                        labelText: "Username",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide(color: Colors.grey)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:BorderRadius.circular(40),
+                          borderSide: BorderSide(color: Colors.blue),
+                        )
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                alignment: Alignment.center,
+                child: Form(
+                  child: TextFormField(
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                        hintText: "Username",
+                        labelText: "Username",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(40),
+                            borderSide: BorderSide(color: Colors.grey)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius:BorderRadius.circular(40),
+                          borderSide: BorderSide(color: Colors.blue),
+                        )
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
